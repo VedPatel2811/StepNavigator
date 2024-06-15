0dba0bc5d967d16e0480bbda2dfb037bd8291317
Ved Patel <138928150+VedPatel2811@users.noreply.github.com>
2024-06-15 04:53:30 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛",
+  "Apply for jobs 💼",
+  "Invest your new income 💰",
+];
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setOpen] = useState(true);
+
+  function previousButton() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function nextButton() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={previousButton}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={nextButton}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}{" "}
+    </>
+  );
+}
